26edcf07073409787828b0af6d2e50179361a45f
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-02-08 17:31:36 +0100

    Applied translation

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,16 @@
 library;
 
+import 'package:code_judge_teacher/l10n/app_localizations.dart';
 import 'package:code_judge_teacher/ui_elements/my_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 int selectedIndexInNavigationBar = 0;
 
 List<MyNavigationBarItemData> getNavigationBarItems(BuildContext context) {
+  final appLocalizations = AppLocalizations.of(context)!;
   return [
-    MyNavigationBarItemData(icon: Icons.code, label: "Submission"), // Submission
-    MyNavigationBarItemData(icon: Icons.code, label: "Exercises"), // Exercises
-    MyNavigationBarItemData(icon: Icons.settings, label: "Settings"), // Settings
+    MyNavigationBarItemData(icon: Icons.code, label: appLocalizations.submissions), // Submission
+    MyNavigationBarItemData(icon: Icons.code, label: appLocalizations.exercises), // Exercises
+    MyNavigationBarItemData(icon: Icons.settings, label: appLocalizations.settings), // Settings
   ];
 }
