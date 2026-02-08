c4102c960205c995f769189f2482e3bc37dac344
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-02-08 19:41:47 +0100

    Implemented settings

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:code_judge_teacher/l10n/app_localizations.dart';
 import 'package:code_judge_teacher/layouts/desktop_layout.dart';
 import 'package:code_judge_teacher/layouts/mobile_layout.dart';
 import 'package:code_judge_teacher/layouts/tablet_layout.dart';
+import 'package:code_judge_teacher/utils/my_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // Load the settings
+  final settingsProvider = SettingsController();
+  await settingsProvider.loadSettings;
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => settingsProvider,
+      child: const MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +25,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Define the settings
+    final settingsController = Provider.of<SettingsController>(context);
     // Define the app theme
     final ThemeData lightTheme = ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime, brightness: Brightness.light),
@@ -30,6 +42,8 @@ class MyApp extends StatelessWidget {
       title: 'CodeJudge',
       theme: lightTheme,
       darkTheme: darkTheme,
+      themeMode: settingsController.selectedTheme,
+
       // Apply settings & language
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
@@ -38,7 +52,7 @@ class MyApp extends StatelessWidget {
         AppLocalizations.delegate
       ],
       supportedLocales: const [Locale('en'), Locale('de')],
-      locale: null,
+      locale: settingsController.selectedLocale,
       home: HomepageLayoutHandler(),
     );
   }
@@ -49,6 +63,8 @@ enum ScreenType {mobile, tablet, desktop}
 
 // Apply the correct layout depending on the screen-type
 class HomepageLayoutHandler extends StatelessWidget {
+  const HomepageLayoutHandler({super.key});
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
