9edc2e42dfab53f40a9279e210865a3cc19bff9c
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-02-08 17:31:24 +0100

    Added translation

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,58 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get submissions => 'Abgaben';
+
+  @override
+  String get exercises => 'Exercises';
+
+  @override
+  String get settings => 'Einstellungen';
+
+  @override
+  String get themeMode => 'Modus des Themas:';
+
+  @override
+  String get system => 'System';
+
+  @override
+  String get lightMode => 'Hell';
+
+  @override
+  String get darkMode => 'Dunkel';
+
+  @override
+  String get localisationMode => 'Sprache:';
+
+  @override
+  String get localeEnglish => 'Englisch';
+
+  @override
+  String get localeGerman => 'Deutsch';
+
+  @override
+  String get aboutThisButton => 'Über dieses Projekt';
+
+  @override
+  String get reportABugButton => 'Einen Bug melden';
+
+  @override
+  String get requestAFeatureButton => 'Ein Feature beantragen';
+
+  @override
+  String get seeLicenseButton => 'Lizenz ansehen';
+
+  @override
+  String get seeReleasesButton => 'Alle Versionen ansehen';
+
+  @override
+  String get aboutMeButton => 'Über den Entwickler';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,58 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get submissions => 'Submissions';
+
+  @override
+  String get exercises => 'Exercises';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get themeMode => 'Theme Mode:';
+
+  @override
+  String get system => 'System';
+
+  @override
+  String get lightMode => 'Light';
+
+  @override
+  String get darkMode => 'Dark';
+
+  @override
+  String get localisationMode => 'Language:';
+
+  @override
+  String get localeEnglish => 'English';
+
+  @override
+  String get localeGerman => 'German';
+
+  @override
+  String get aboutThisButton => 'About this project';
+
+  @override
+  String get reportABugButton => 'Report a bug';
+
+  @override
+  String get requestAFeatureButton => 'Request a feature';
+
+  @override
+  String get seeLicenseButton => 'See the license';
+
+  @override
+  String get seeReleasesButton => 'See all releases';
+
+  @override
+  String get aboutMeButton => 'About the developer';
+}
